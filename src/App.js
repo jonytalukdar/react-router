929d6afee550ae7753af7e4fe74649fad205fe47
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-06 23:38:49 +0600

    added react router and create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Home from './componets/home/Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import NoMatch from './componets/noMatch/NoMatch';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NoMatch></NoMatch>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/componets/friend/Friend.js
+++ src/componets/friend/Friend.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './friend.css';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
 const Friend = (props) => {
   const { id, name, email } = props.friend;
   console.log(props);
