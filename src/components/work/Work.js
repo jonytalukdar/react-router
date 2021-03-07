fdc7657538cdd3a66c1eb3737f22edb522e87080
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-07 17:00:51 +0600

    added cart form more information

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import FakeDate from '../../fakeData/works.json';
+import { Card, Button } from 'react-bootstrap';
 
 const Work = () => {
   return (
     <div>
-      <h1>this is my work page</h1>
+      {FakeDate.map((list) => (
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={list.image} />
+          <Card.Body>
+            <Card.Title>
+              <h1>{list.name}</h1>
+            </Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">View Details</Button>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 };
