387ff3bcf394572d5b3d238e1fba91140c403bbf
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-07 11:15:21 +0600

    remome some file to start again

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
 import './App.css';
-import Home from './componets/home/Home';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import NoMatch from './componets/noMatch/NoMatch';
-import FriendDetail from './componets/friendDetails/FriendDetail';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/friend/:friendId">
-            <FriendDetail></FriendDetail>
-          </Route>
-          <Route path="*">
-            <NoMatch></NoMatch>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
