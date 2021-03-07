255d3227e7f5206d65f6177cf37491887ebdc6d7
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-07 19:51:49 +0600

    added some file

--- src/App.js
+++ src/App.js
@@ -1,44 +1,72 @@
-import React from 'react';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Works from './components/Works';
+import Contact from './components/Contact';
+import WorkDetail from './components/WorkDetail';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import About from './components/about/About';
-import Home from './components/home/Home';
-import Portfolio from './components/portfolio/Portfolio';
-import Work from './components/work/Work';
-import NotFound from './components/notFound/NotFound';
-
-const App = () => {
+const address = "/about"
+switch(address) {
+  case '/home':
+    // Home page dekhate hobe
+    break;
+  case '/about':
+    // About page dekhate hobe
+    break;
+  case '/works':
+      // Works page dekhate hobe
+    break;
+  case '/contact':
+      // Contact page dekhate hobe
+    break;
+  default: 
+    // 404
+  
+}
+function App() {
   return (
-    <div>
-      <Router>
-        <Header></Header>
+    <>
+    <Router>
+      <Header />
 
-        <Switch>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/about">
-            <About></About>
-          </Route>
-          <Route path="/portfolio">
-            <Portfolio></Portfolio>
-          </Route>
-          <Route path="/work">
-            <Work></Work>
-          </Route>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-        <Footer></Footer>
-      </Router>
-    </div>
+      <Switch>
+        
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/works">
+          <Works />
+        </Route>
+        <Route  path="/work/:productName">
+          <WorkDetail />
+        </Route>
+
+        <Route path="/contact">
+          <Contact />
+        </Route>
+
+        <Route path="*">
+          <h1>404 Not found!</h1>
+        </Route>
+        
+      </Switch>
+      <Footer />
+    </Router>
+    </>
   );
-};
+}
 
 export default App;
