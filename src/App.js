907f7f9f748d33a14ba50a6c3ee60f4d7ad964a5
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-07 15:19:18 +0600

    remome all file to be continoue

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './components/home/Home';
-import NotFound from './components/notFound/NotFound';
-import CountryDetail from './components/countryDetail/CountryDetail';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/country/:name">
-            <CountryDetail></CountryDetail>
-          </Route>
-          <Route path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
