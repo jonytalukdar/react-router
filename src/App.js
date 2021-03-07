e58087f508c108c4cb94763b3c1b252f86e1c9d0
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-07 11:27:48 +0600

    connect all route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/home/Home';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
