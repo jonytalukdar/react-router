8c99749d6ea34820df1b02ef3c5a3946d61d817e
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-07 16:07:50 +0600

    link all components

---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>this is my portfolio page</h1>
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/components/work/Work.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Work = () => {
+  return (
+    <div>
+      <h1>this is my work page</h1>
+    </div>
+  );
+};
+
+export default Work;
